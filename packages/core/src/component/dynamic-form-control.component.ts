5a9837599bf7b740f44de73d0f721bfa7c077e25
MHR\maxime <maxime@mercury.com.au>
2017-12-11 14:46:40 +1100

    Removing comments

--- packages/core/src/component/dynamic-form-control.component.ts
+++ packages/core/src/component/dynamic-form-control.component.ts
@@ -162,19 +162,12 @@ export abstract class DynamicFormControlComponent implements OnChanges, OnInit, 
 					this.onModelHiddenUpdates(RelationUtils.isFormControlToBeHiddenByWorkflow(eRelation, this.workflowActions));
 					this.onModelDisabledUpdates(RelationUtils.isFormControlToBeDisabledByWorkflow(eRelation, this.workflowActions));
 				});
-				// let requestorRelations = this.model.workflowRelation.filter(c => c.group.id === "Requestor");
-				// requestorRelations.forEach(rRelation => {
-					// this.onModelHiddenUpdates(RelationUtils.isFormControlToBeHiddenByWorkflow(rRelation, this.workflowActions));
-					// this.onModelDisabledUpdates(RelationUtils.isFormControlToBeDisabledByWorkflow(rRelation, this.workflowActions));
-				// });
 				if (this.userGroups && this.userGroups.length > 0) {
 					this.userGroups.forEach(group => {
 						let groupRelations = [];
 						if (group.name === "Requestor") {
-							console.log("ÇHECKING RELATIONS FOR REQUESTOR");
 							groupRelations = this.model.workflowRelation.filter(c => c.group.id === "Requestor");
 						} else {
-							console.log("ÇHECKING RELATIONS FOR ", group);
 							groupRelations = this.model.workflowRelation.filter(c => c.group.id === group.id);
 						}
 						
