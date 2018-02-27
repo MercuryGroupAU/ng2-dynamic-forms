b9bcdf135a457ec292203d5636798c27ea26fd3f
Dan <daniel@mercury.com.au>
2018-02-27 12:07:43 +1100

    updating gulp for ngc

--- build/tasks/ngc.js
+++ build/tasks/ngc.js
@@ -1,3 +1,15 @@
-const run = require("gulp-run");
+const run = require("gulp-run"),
+    ngc = require('@angular/compiler-cli/src/main').main;
 
-module.exports = tsConfigPath => () => run(`ngc -p ${tsConfigPath}`).exec();
+module.exports = tsConfigPath => () => {
+    //console.log('tsConfigPath', tsConfigPath);
+    return ngc({
+        project: `${tsConfigPath}`
+    }).then((exitCode) => {
+        if (exitCode === 1) {
+            // This error is caught in the 'compile' task by the runSequence method callback
+            // so that when ngc fails to compile, the whole compile process stops running
+            throw new Error('ngc compilation failed');
+        }
+    });
+}
